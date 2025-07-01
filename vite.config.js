6ebc8373adaaf76c3883fc1cedf003d041244dfa
Sholihul Fadjri Triwibowo <88471883+Fdjri@users.noreply.github.com>
2025-07-01 16:29:30 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,4 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    build: {
-        outDir: 'public/build',
-        assetsDir: 'assets',
-        manifest: true,
-    },
 });
